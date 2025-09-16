ccb9cc5d9d0902b35728236747a2972f007e6e2b
Nicholas Eldergill <Nicholas.Eldergill@city.ac.uk>
2025-09-16 16:14:41 +0100

    add comment routes, added both tickets and comments to app.js

--- Incident Reporting/server/app.js
+++ Incident Reporting/server/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 
+const ticketRouter = require('./routers/ticket')
+const commentRouter = require('./routers/comment')
+
 const logger = require("");
 
 const app = express();
 app.use(express.json())
 app.use(cors());
 app.use(logger);
+
+app.use('/ticket', ticketRouter)
+app.use('/comment', commentRouter)
 
 app.get("/", (req, res) => {
   res.status(200).json({
--- Incident Reporting/server/models/Comments.js
+++ Incident Reporting/server/models/Comments.js
@@ -58,3 +58,5 @@ class Comment{
         }
     }
 }
+
+module.exports = Comment
