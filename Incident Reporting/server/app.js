b1bb8f94caecaf8985d509909d58305c7cf71483
Samuel Pipi <samuelpipi82@gmail.com>
2025-09-17 10:50:38 +0100

    add functionality to handle request to app at endpoints relating to department and orgs

--- Incident Reporting/server/app.js
+++ Incident Reporting/server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
-const ticketRouter = require('./routers/ticket')
-const commentRouter = require('./routers/comment')
 
 const logger = require("./middleware/logger");
+
+const ticketRouter = require('./routers/ticket')
+const commentRouter = require('./routers/comment')
+const organisationRouter = require("./routers/organisations");
+const userRouter = require("./routers/users");
+const departmentRouter = require("./routers/department");
 
 const app = express();
 app.use(express.json())
@@ -13,6 +17,9 @@ app.use(logger);
 
 app.use('/ticket', ticketRouter)
 app.use('/comment', commentRouter)
+app.use("/user", userRouter);
+app.use("/organisation", organisationRouter);
+app.use('/department', departmentRouter)
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -21,6 +28,5 @@ app.get("/", (req, res) => {
   })
 })
 
-app.use("/user", userRouter);
 
 module.exports = app;
