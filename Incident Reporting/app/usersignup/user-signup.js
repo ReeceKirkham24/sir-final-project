4649c4f1f96a6ca5e3ebe7819ad43f15877148f7
Nicholas Eldergill <Nicholas.Eldergill@city.ac.uk>
2025-09-18 14:38:11 +0100

    add user signup functionality

---
+++ Incident Reporting/app/usersignup/user-signup.js
@@ -0,0 +1,30 @@
+document.getElementById("signup-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const options = {
+        method: "POST",
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            org_id: 1,
+            department_id: 1,
+            password_hash: password,
+        })
+    }
+    console.log(options);
+    const response = await fetch("http://localhost:3000/user", options)
+    const data = await response.json();
+
+    if (response.status == 201) {
+    window.location.assign("/Incident Reporting/app/Usercontentpage/usercontent.html")
+    } else {
+        alert(data.error);
+    }
+})
